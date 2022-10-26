f29b300ccf23d6e7386b77cd2ce2624ea5474e69
Arditti93 <alex.arditti@hotmail.com>
2022-10-26 11:06:35 +0100

    added styling and clean up

--- src/App.jsx
+++ src/App.jsx
@@ -13,23 +13,29 @@ const submitHandler = async (event) => {
   await register (username, email, password)
 }
   return (
-    <div>
-      <h1>ME NO LIKE CSS</h1>
+    <div className="container">
+      <h1>Register users</h1>
     
       <form onSubmit ={submitHandler}>
             <label> Username:
+                <br></br>
                 <input onChange={(event) => setUsername(event.target.value)} />
             </label>
             <br></br>
+            <br></br>
 
             <label> Email:
+                <br></br>
                 <input onChange={(event) => setEmail(event.target.value)} />
             </label>
             <br></br>
+            <br></br>
 
-            <label> password:
+            <label> Password:
+                <br></br>
                 <input onChange={(event) => setPassword(event.target.value)} />
             </label>
+            <br></br>
             <br></br>
             <button type='submit'>Click here to register</button>
         </form>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
 export const register = async (username, email, password) => {
     console.log(username, email, password)
     try {
-        const response = await fetch(`${process.env.REACT_APP_REST_API}addUser`, {
+        const response = await fetch(`${process.env.REACT_APP_API}`, {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
